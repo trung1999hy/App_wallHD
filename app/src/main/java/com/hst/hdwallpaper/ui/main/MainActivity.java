86a1af279b19f74b21ebabda3fe9aa1fa75f61bf
Tran nam <anhnam2.2k.yn@gmail.com>
2023-08-24 23:17:10 +0700

    fix coin

--- app/src/main/java/com/hst/hdwallpaper/ui/main/MainActivity.java
+++ app/src/main/java/com/hst/hdwallpaper/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hst.hdwallpaper.ui.main;
 
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hst.hdwallpaper.App;
 import com.hst.hdwallpaper.R;
 import com.hst.hdwallpaper.data.base.BaseActivity;
 import com.hst.hdwallpaper.ui.category.CategoryFragment;
@@ -35,20 +37,20 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter> implemen
         this.fragmentContainer = findViewById(R.id.fragment_container);
         this.toolbar = findViewById(R.id.toolbar);
         this.toolbarTitles = findViewById(R.id.toolbar_titles);
-
+        this.pointLayout = findViewById(R.id.pointView);
         this.pointWallet = findViewById(R.id.pointWallet);
-
-
         this.presenter.initView(this, this.bottomNavigation);
         this.toolbar.setTitle("");
         this.toolbarTitles.setText(getString(R.string.toolbar_title));
         setSupportActionBar(this.toolbar);
+        pointLayout.setVisibility(View.GONE);
         callFragment(R.id.fragment_container, new RecentFragment());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        pointWallet.setText(App.getInstance().getValueCoin() + "");
     }
 
     public void onDestroyed() {
@@ -64,15 +66,16 @@ public class MainActivity extends BaseActivity<MainView, MainPresenter> implemen
 
     public void onItemClick(int itemIndex) {
         if (itemIndex == R.id.home) {
+            pointLayout.setVisibility(View.GONE);
             callFragment(R.id.fragment_container, new RecentFragment());
-
         } else if (itemIndex == R.id.categories) {
+            pointLayout.setVisibility(View.GONE);
             callFragment(R.id.fragment_container, new CategoryFragment());
-
         } else if (itemIndex == R.id.favourite) {
-
+            pointLayout.setVisibility(View.GONE);
             callFragment(R.id.fragment_container, new FavouriteFragment());
         } else if (itemIndex == R.id.coin) {
+            pointLayout.setVisibility(View.VISIBLE);
             inApp();
         }
     }
